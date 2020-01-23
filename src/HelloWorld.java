dddc729ad47a601917b06561c43ca73bf5df4172
Zakaria AFKIR <z.afkir.externe@polyexpert.fr>
2020-01-23 12:48:10 +0100

    Format with Eclipse

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        String str = "abcdefghijkl";
-        System.out.println(str.substring(3, 12));
-    }
+	public static void main(String[] args) {
+		String str = "abcdefghijkl";
+		System.out.println(str.substring(3, 12));
+	}
 }
