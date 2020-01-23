6e05321c43905632663ca3b5cc9ee658140c1351
Zakaria AFKIR <z.afkir.externe@polyexpert.fr>
2020-01-23 13:13:45 +0100

    Say Bye

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,4 +7,8 @@ public class HelloWorld {
 	public static void sayHello() {
         System.out.println("Hello World!!!!");
 	}
+
+	public static void sayBye() {
+        System.out.println("Bye.");
+    }
 }
