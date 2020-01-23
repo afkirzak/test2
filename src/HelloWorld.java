1cfd431a5feb0cee54bd9c80430130ebc6832f2e
Zakaria AFKIR <z.afkir.externe@polyexpert.fr>
2020-01-23 12:46:18 +0100

    Modify substring method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         String str = "abcdefghijkl";
-        System.out.println(str.substring(3, 10));
+        System.out.println(str.substring(3, 12));
     }
 }
