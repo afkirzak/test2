05115c747896e0575a6fd866fad57f0b85429de9
Zakaria AFKIR <z.afkir.externe@polyexpert.fr>
2020-01-23 13:55:31 +0100

    Second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,13 +2,21 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		sayHello();
+		sayBye();
+
+		sayHello();
 	}
 
 	public static void sayHello() {
-        System.out.println("Hello World!!!!");
+		System.out.println("Hello World!!!!");
 	}
 
 	public static void sayBye() {
-        System.out.println("Bye.");
-    }
+		System.out.println("Bye.");
+
+	}
+
+	public static void sayHowAreU() {
+		System.out.println("How are u?");
+	}
 }
